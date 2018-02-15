b8c49dbfaa9eeecfe32411d830014ac379c89655
Steve Tyler <mail@steve-tyler.co.uk>
2018-02-15 22:03:22 +0000

    added check for integer as NaN was being returned

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,13 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
 server.get('/:number', function (req, res, next) {
-  if (typeof req.params.number !== 'number') {
+  const num = parseInt(req.params.number, 10);
+
+  if (!Number.isInteger(num)) {
+    res.send('wrong request');
     res.status(400);
     return;
   }
-
-  const num = parseInt(req.params.number, 10);
-  console.log(num, typeof(num));
 
   const response = {
     nearest_prime_number: nearestPrime(num as number)
@@ -23,7 +23,7 @@ server.get('/:number', function (req, res, next) {
   return;
 });
 
-server.listen(8080, function () {
+server.listen(3000, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
 
@@ -33,7 +33,6 @@ function nearestPrime(num: number) {
   let i = 1;
   let testNumber = num;
 
-  //console.log('testNumber', num);
   if(isPrime(testNumber)) {
     return testNumber;
   }
@@ -65,18 +64,20 @@ function nearestPrime(num: number) {
   }
 }
 
-
 /* Unit Tests */
-console.log('\n** unit tests ** \n' );
+console.log('\n** Unit Tests ** \n' );
 
-console.log(nearestPrime(1) === 2, nearestPrime(1), 'nearestPrime(1)');
-console.log(nearestPrime(2) === 2, nearestPrime(2), 'nearestPrime(2)');
-console.log(nearestPrime(3) === 3, nearestPrime(3), 'nearestPrime(3)');
-console.log(nearestPrime(4) === '3,5', nearestPrime(4), 'nearestPrime(4)');
-console.log(nearestPrime(8) === 7, nearestPrime(8), 'nearestPrime(8)');
-console.log(nearestPrime(14) === 13, nearestPrime(14), 'nearestPrime(14)');
-console.log(nearestPrime(26) === '23,29', nearestPrime(26), 'nearestPrime(26)');
-console.log(nearestPrime(36) === 37, nearestPrime(36), 'nearestPrime(36)');
-console.log(nearestPrime(38) === 37, nearestPrime(38), 'nearestPrime(38)');
-console.log(nearestPrime(101) === 101, nearestPrime(101), 'nearestPrime(101)');
-console.log(nearestPrime(4782739287) === 4782739267, nearestPrime(4782739287), 'nearestPrime(4782739287)');
+console.log(nearestPrime(1) === 2, '1', nearestPrime(1));
+console.log(nearestPrime(2) === 2, '2', nearestPrime(2));
+console.log(nearestPrime(3) === 3, '3', nearestPrime(3));
+console.log(nearestPrime(4) === '3,5', '4', nearestPrime(4));
+console.log(nearestPrime(8) === 7, '8', nearestPrime(8));
+console.log(nearestPrime(14) === 13, '14', nearestPrime(14));
+console.log(nearestPrime(26) === '23,29', '26', nearestPrime(26));
+console.log(nearestPrime(36) === 37, '36', nearestPrime(36));
+console.log(nearestPrime(38) === 37, '38', nearestPrime(38));
+console.log(nearestPrime(101) === 101, '101', nearestPrime(101));
+console.log(nearestPrime(1401) === 1399, '1401', nearestPrime(1401));
+console.log(nearestPrime(96481) === 96479, '96481', nearestPrime(96481));
+console.log(nearestPrime(100621) === 100621, '100621', nearestPrime(100621));
+console.log(nearestPrime(4782739287) === 4782739267, '4782739287', nearestPrime(4782739287));
